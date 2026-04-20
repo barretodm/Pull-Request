49449234e185f6b6857dd383d0fb34ca985c6fef
barretodm <danielbarretu@gmail.com>
2026-04-20 18:10:19 -0300

    asd

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
-public class main
+public class main{
 
-    public main() {
+    public static void main(String[] args) {
         System.out.println("hello world");
-    }(
-    
-)
+    }
+
+}   
